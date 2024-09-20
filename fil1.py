22a513c855dda507077dbb636327c8c1f3e0831d
podysseas <odysseas.polycarpou@gmail.com>
2024-09-20 22:18:45 +0300

    complete replacing the branch name in all files with the one specified by the user

---
+++ fil1.py
@@ -0,0 +1,22 @@
+# This is file1.py
+
+
+
+def greet():
+
+    print("Hello from TRT!")
+
+
+
+def info():
+
+    print("TRT stands for Next-Gen Python Global Library.")
+
+
+
+if __name__ == "__main__":
+
+    greet()
+
+    info()
+
---
+++ fil2.py
@@ -0,0 +1,20 @@
+# This is file2.py
+
+
+
+from fil1 import greet
+
+
+
+def farewell():
+
+    print("Goodbye from TRT!")
+
+
+
+if __name__ == "__main__":
+
+    greet()
+
+    farewell()
+
